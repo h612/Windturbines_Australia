d6a5ff608e20180aae2837f6938c60b420f9e3cf
hcfugro <h.chaudhry@fugro.com>
2022-01-27 00:03:43 +1100

    Map update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,16 @@
-var myMap = L.map("map", {
-  center: [40.7128, -74.0059],
-  zoom: 2
-});
+// var myMap = L.map("map", {
+//   center: [40.7128, -74.0059],
+//   zoom: 2
+// });
+// map.invalidateSize();
 
-// // Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.satellite",
-    accessToken: API_KEY
-}).addTo(myMap);
+// // // Adding tile layer
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//     maxZoom: 18,
+//     id: "mapbox.satellite",
+//     accessToken: API_KEY
+// }).addTo(myMap);
 
 // Earthquakes GeoJSON URL Variables
 console.log("HERE!!")
